6df8957a2170884718fe7b2dc5302daec0439915
iTrix <marwan.mezroui.pro@gmail.com>
2023-04-14 11:35:45 +0200

    feat: add UI folder

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Home page</h1>
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+  <router-link to="/quiz">Démarrer le quiz !</router-link>
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores: []
+    };
+  },
+  async created() {
+    registeredScores = await quizApiService.getQuizInfo();
+  }
+};
+</script>
+
+<style>
+
+</style>
